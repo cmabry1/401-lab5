2111439b1021ca53ffca902ad860c43072a0370c
cmabry1 <143213754+cmabry1@users.noreply.github.com>
2026-02-15 19:24:08 -0600

    Add Queue class implementation in Queue.hpp

---
+++ Queue.hpp
@@ -0,0 +1,29 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+
+#include <iostream>
+using namespace std;
+
+class Queue {
+private:
+    int* arr;
+    int capacity;
+    int frontIndex;
+    int rearIndex;
+    int size;
+
+public:
+    // Constructor
+    Queue(int cap);
+
+    // Destructor
+    ~Queue();
+
+    void enqueue(int value);
+    int dequeue();
+    int front();
+    bool isEmpty();
+    bool isFull();
+};
+
+#endif
